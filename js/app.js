68abbe510b48b8aa89c77294da0a150117c14bec
ulyss204 <rym1112ov@gmail.com>
2015-08-20 14:31:30 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -97,15 +97,7 @@ Player.prototype.handleInput = function(key){
     if (this.y>100){
         thi.reset();
     }
-    player = addEventListener('touchmove', function(event) {
-  // Если 1 палец внутри элемента
-        if (event.targetTouches.length == 1) {
-        var touch = event.targetTouches[0];
-    // Place element where the finger is
-    obj.style.left = touch.pageX + 101;
-    obj.style.top = touch.pageY + 83;
-  }
-}, false);
+    
 }
 
 // Now instantiate your objects.
